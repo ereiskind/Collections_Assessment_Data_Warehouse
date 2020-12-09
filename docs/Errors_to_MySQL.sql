63f227901cc12384d85409be0a143f4542e4fbaf
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 17:06:03 +0000

    Create statement to create database

--- docs/Errors_to_MySQL.sql
+++ docs/Errors_to_MySQL.sql
@@ -1,5 +1,7 @@
 -- This file exists to record the SQL created for the idea of tracking problems that emerged in report harvesting in MySQL, which was moved away from before the this repository became the host of the source code.
 
+CREATE SCHEMA 'testdatawarehouse';
+
 CREATE TABLE SUSHIErrorLog (
     SUSHIErrorLog_ID SERIAL,
     Report_ID BIGINT,

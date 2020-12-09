f9f4a99c31861ce22de8d968855b0d28edbd3efe
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 23:11:47 +0000

    Move API call helper functions to SUSHI_R5_API_Calls

--- app.py
+++ app.py
@@ -68,58 +68,6 @@ def Execute_SQL_Statement(SQLStatement, DBConnection):
     DBConnection.commit()
 
 
-#Subsection: Data Retrieval from Web
-def Retrieve_Downloaded_JSON_File(WebDriver, URL):
-    """Reads a JSON downloaded by an API call into memory.
-    
-    Some of the SUSHI API calls, most notable Silverchair, generate a JSON file download, which requests returns as a 403 error. This function captures the downloaded file and reads its contents into memory so the function can be used. Functionality related to downloading the file taken from https://medium.com/@moungpeter/how-to-automate-downloading-files-using-python-selenium-and-headless-chrome-9014f0cdd196.
-
-    Arguments:
-        browser {WebDriver} -- Selenium WebDriver used to access internet
-        download_dir {File path} -- location the file should be downloaded to
-        WebDriver {WebDriver} -- Selenium WebDriver used to access internet
-        URL {string} -- the URL for performing the API call
-    
-    Returns:
-        dictionary -- the API response JSON file transformed into Python data types
-    """
-    # The function requires a string containing the absolute path to the location where the file should be saved; this allows for a folder "API_Download" within the repo to hold the file
-    API_Download_Folder = str(Path.cwd()) + r"\API_Download"
-
-    # From source: "function to handle setting up headless download"
-    WebDriver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-    params = {'cmd':'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': API_Download_Folder}}
-    WebDriver.execute("send_command", params)
-    WebDriver.get(URL) # From source: "get request to target the site selenium is active on"
-
-    time.sleep(0.1) # This delay allows the downloaded JSON to be in the folder for long enough that the walk method can detect it
-    for Folder, Subfolder, Files in os.walk(API_Download_Folder):
-        for File in Files: # There is actually only one file, but the iterator is needed to extract it from the list data structure
-            Download_File_Path = str(Path('.', 'API_Download', File))
-            with open(Download_File_Path) as JSONfile:
-                JSON_File_Data = json.load(JSONfile)
-            os.unlink(Download_File_Path)
-
-    return JSON_File_Data
-
-
-#Subsection: Data Type Conversions
-def JSON_to_Python_Data_Types(JSON):
-    """Returns a JSON-like object that uses Python data types from multiple different forms of input that include JSON formatted data.
-    This function takes a JSON-like Python dictionary, JSON-like Python list, requests object with JSON data, or <insert other objects here> and returns the same data using native Python data types.
-    Arguments:
-        JSON {varies} -- an object containing a JSON or JSON-like data
-    Returns:
-        dictionary or list -- the data in JSON using Python data types
-    """
-    if str(type(JSON)) == "<class 'dict'>":
-        return JSON
-    elif str(type(JSON)) == "<class 'list'>": #Alert: Not yet tested
-        return JSON
-    elif str(type(JSON)) == "<class 'requests.models.Response'>":
-        return JSON.json()
-
-
 #Section: Establish Prerequisites for Script Execution
 #Subsection: Confirm Folder "API_Download" is Empty
 API_Download_Path = str(Path.cwd()) + r"\API_Download"

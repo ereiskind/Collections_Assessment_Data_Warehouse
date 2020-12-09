28444c35e095020ab399f5964b32e5f2ef6d4ee5
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 18:05:40 +0000

    Create Credentials_Through_Alma_API

---
+++ helpers/Credentials_Through_Alma_API.py
@@ -0,0 +1,9 @@
+# Alma API starting docs: https://developers.exlibrisgroup.com/alma/apis/
+# Path for GET vendor API call: https://developers.exlibrisgroup.com/alma/apis/acq/#Vendors
+# Statistics section of return for GET vendor: https://developers.exlibrisgroup.com/alma/apis/docs/xsd/rest_vendor.xsd/?tags=GET#statistics
+
+# User ID: Customer ID
+# Password: Requestor ID
+# User Password Note: API Key
+
+# Vendor code for testing: JSTOR

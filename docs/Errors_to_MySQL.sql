bc7ca25128775c7818a756868163a66816aa171f
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 16:59:01 +0000

    Create files for table DDL statements

---
+++ docs/Errors_to_MySQL.sql
@@ -0,0 +1,9 @@
+-- This file exists to record the SQL created for the idea of tracking problems that emerged in report harvesting in MySQL, which was moved away from before the this repository became the host of the source code.
+
+CREATE TABLE SUSHIErrorLog (
+
+); 
+
+CREATE TABLE SUSHIErrorReports (
+
+); 

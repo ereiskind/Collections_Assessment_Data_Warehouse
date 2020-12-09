f7a29611f9108c180a55d149dc853c7e55e14f38
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 21:17:49 +0000

    Create Authentication_Log_Collection with notes

---
+++ helpers/Authentication_Log_Collection.py
@@ -0,0 +1,11 @@
+# Collecting information about authentications to platforms--for approximating how widely distributed the usage of the resources on a platform are
+
+#Section: EZProxy
+# Since this captured only off-campus usage, will this be more misleading than useful?
+# What type of logs have been kept?
+# Potentially needing to dedupe before stripping out identifying information
+# Default logs all activity, not just initial platform authentications
+
+#Section: OpenAthens
+# API documentation: https://docs.openathens.net/display/public/MD/Fetching+statistics+reports+via+the+API
+# API access costs money and is managed at level of OA contract--the licensing body controls API access

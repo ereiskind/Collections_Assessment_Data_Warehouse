100815f75cfef0d4a70fc71605a3afbeaad109a8
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-09 17:03:03 +0000

    Frame DDL for creating SUSHIErrorLog

--- docs/Errors_to_MySQL.sql
+++ docs/Errors_to_MySQL.sql
@@ -1,7 +1,12 @@
 -- This file exists to record the SQL created for the idea of tracking problems that emerged in report harvesting in MySQL, which was moved away from before the this repository became the host of the source code.
 
 CREATE TABLE SUSHIErrorLog (
-
+    SUSHIErrorLog_ID SERIAL,
+    Report_ID BIGINT,-- Foreign key connected to SUSHIErrorReports.SUSHIErrorReports_ID
+    Error_Code VARCHAR(4),
+    Error_Details VARCHAR(75),
+    Error_Name VARCHAR(65),
+    Severity VARCHAR(10)
 ); 
 
 CREATE TABLE SUSHIErrorReports (

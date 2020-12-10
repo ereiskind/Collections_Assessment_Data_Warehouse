352ab3e318352a7161098fc801e78b043481817c
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-10 17:25:48 +0000

    Add not null constraints to create table statements

--- data/Data_Warehouse_DDL_Statements.sql
+++ data/Data_Warehouse_DDL_Statements.sql
@@ -1,16 +1,16 @@
 CREATE TABLE LIB_Vendors (
-    CORAL_ID INT PRIMARY KEY,
-    Vendor_Name VARCHAR(75)
+    CORAL_ID INT PRIMARY KEY NOT NULL,
+    Vendor_Name VARCHAR(75) NOT NULL
 ); 
 
 CREATE TABLE LIB_Interfaces (
-    Interfaces_ID INT PRIMARY KEY, -- Should this be pre-supplied to ensure matching with JSON and mimic production, where values from Alma will likely be used?
-    Interface_Name VARCHAR(75),
-    COUNTER_R4_Compliant BOOLEAN,
-    SUSHI_R5_Compliant BOOLEAN,
-    Has_Usage_Portal BOOLEAN,
+    Interfaces_ID INT PRIMARY KEY NOT NULL, -- Should this be pre-supplied to ensure matching with JSON and mimic production, where values from Alma will likely be used?
+    Interface_Name VARCHAR(75) NOT NULL,
+    COUNTER_R4_Compliant BOOLEAN NOT NULL,
+    SUSHI_R5_Compliant BOOLEAN NOT NULL,
+    Has_Usage_Portal BOOLEAN NOT NULL,
     Email_for_Usage VARCHAR(50),
-    Vendor INT,
+    Vendor INT NOT NULL,
     INDEX INDX_Vendor (Vendor),
     CONSTRAINT FK_Interfaces_Vendor FOREIGN KEY INDX_Vendor (Vendor)
         REFERENCES LIB_Vendors(CORAL_ID)
@@ -19,8 +19,8 @@ CREATE TABLE LIB_Interfaces (
 );
 
 CREATE TABLE LIB_Fiscal_Year (
-    Fiscal_Year_ID INT PRIMARY KEY,
-    Fiscal_Year CHAR(9)
+    Fiscal_Year_ID INT PRIMARY KEY NOT NULL,
+    Fiscal_Year CHAR(9) NOT NULL
 );
 
 CREATE TABLE LIB_Stats_Collection_Info (

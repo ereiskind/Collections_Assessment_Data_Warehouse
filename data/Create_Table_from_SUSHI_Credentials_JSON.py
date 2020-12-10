898eaf90269733b5706c559bb9adc2eb88435884
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-10 02:58:23 +0000

    Specify issues in SUSHI_R5_Credentials_Template

--- data/Create_Table_from_SUSHI_Credentials_JSON.py
+++ data/Create_Table_from_SUSHI_Credentials_JSON.py
@@ -1,7 +1,5 @@
 # This outputs the contents of SUSHI_R5_Credentials.json, a file in the same folder containing a list of JSON object with the same structure as the one shown in SUSHI_R5_Credentials_Template.json, in a tabular format
 
-#ToDo: Develop way to indicate if issue indicated in JSON is being handled appropriately by scripts or needs more attention
-
 import json
 import csv
 
@@ -9,9 +7,12 @@ fileIO = open('SUSHI_R5_Credentials.csv', 'w', newline='')
 CSV_Writer = csv.DictWriter(fileIO, [
     "Platform/Stats Source",
     "JSON Name",
+    "Problem Needs Attention",
     "Vendor_Name",
     "Covered Platforms",
-    "Collection Issues",
+    "Added to Platforms_Not_Collected",
+    "Other Issue Info",
+    "Issue Date",
     "Ready Notice Email",
     "URL",
     "Customer ID",
@@ -19,6 +20,7 @@ CSV_Writer = csv.DictWriter(fileIO, [
     "API Key",
     "SUSHI Platform Code",
     "CORAL",
+    "Potential Timeout",
 ])
 CSV_Writer.writeheader()
 
@@ -45,8 +47,16 @@ with open('SUSHI_R5_Credentials.json') as JSON_File:
                 Stats_Source["API Key"] = Interface["statistics"]["delivery_address"]
             if Interface["statistics"]["locally_stored"] != "":
                 Stats_Source["SUSHI Platform Code"] = Interface["statistics"]["locally_stored"]
-            if Interface["not from Alma"]["issues"] != "":
-                Stats_Source["Collection Issues"] = Interface["not from Alma"]["issues"]
+            if Interface["not from Alma"]["issues"]["timeout"] == True:
+                Stats_Source["Potential Timeout"] = True
+            if Interface["not from Alma"]["issues"]["Platforms_Not_Collected"] != "":
+                Stats_Source["Added to Platforms_Not_Collected"] = Interface["not from Alma"]["issues"]["Platforms_Not_Collected"]
+            if Interface["not from Alma"]["issues"]["info"] != "":
+                Stats_Source["Other Issue Info"] = Interface["not from Alma"]["issues"]["info"]
+            if Interface["not from Alma"]["issues"]["attention"] == True:
+                Stats_Source["Problem Needs Attention"] = True
+            if Interface["not from Alma"]["issues"]["date"] != "":
+                Stats_Source["Issue Date"] = Interface["not from Alma"]["issues"]["date"]
             if Interface["not from Alma"]["notification"] != "":
                 Stats_Source["Ready Notice Email"] = Interface["not from Alma"]["notification"]
             CSV_Writer.writerow(Stats_Source)

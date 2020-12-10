f55716d26b7bd15fb63d7902ee78d839220145b4
Elissa Reiskind <ereiskind@fsu.edu>
2020-12-10 17:23:26 +0000

    Create SQL statement for Stats_Collection_Info table

--- data/Data_Warehouse_DDL_Statements.sql
+++ data/Data_Warehouse_DDL_Statements.sql
@@ -22,3 +22,26 @@ CREATE TABLE LIB_Fiscal_Year (
     Fiscal_Year_ID INT PRIMARY KEY,
     Fiscal_Year CHAR(9)
 );
+
+CREATE TABLE LIB_Stats_Collection_Info (
+    Interface INT NOT NULL,
+    Fiscal_Year INT NOT NULL,
+    Manual_Collection_Required BOOLEAN NOT NULL,
+    Manual_Collection_Completed BOOLEAN,
+    Note TEXT,
+    In_eBooks_Report BOOLEAN NOT NULL,
+    In_eJournals_Report BOOLEAN NOT NULL,
+    In_Databases_Report BOOLEAN NOT NULL,
+    In_Multimedia_Report BOOLEAN NOT NULL,
+    PRIMARY KEY (Interface, Fiscal_Year),
+    INDEX INDX_Interface (Interface),
+    CONSTRAINT FK_SCInfo_Interface FOREIGN KEY INDX_Interface (Interface)
+        REFERENCES LIB_Interfaces(Interfaces_ID)
+        ON UPDATE restrict
+        ON DELETE restrict,
+    INDEX INDX_FY (Fiscal_Year),
+    CONSTRAINT FK_SCInfo_FY FOREIGN KEY INDX_FY (Fiscal_Year)
+        REFERENCES LIB_Fiscal_Year(Fiscal_Year_ID)
+        ON UPDATE restrict
+        ON DELETE restrict
+);

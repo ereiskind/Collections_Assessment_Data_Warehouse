30b3d69cc36b8c3fdcf0f0e8a5046460e361a7af
Elissa_Reiskind <ereiskind@fsu.edu>
2020-12-18 22:23:59 +0000

    Capture exception when unable to load to MySQL

--- app.py
+++ app.py
@@ -484,14 +484,14 @@ for SUSHI_Call_Data in SUSHI_Data:
                 con=Engine
             )
             logging.info(f"Successfully loaded {Master_Report_Type} for {SUSHI_Call_Data['JSON_Name']} into database:\n{Check_Loading.tail()}")
-        except:
+        except Exception as Error_Message:
             Master_Report_Loading_Problem = dict(
                 Interface = SUSHI_Call_Data["JSON_Name"],
                 Type = Master_Report_Type,
-                Details = f"Unable to load {Master_Report_Type} dataframe to MySQL",
+                Details = f"Unable to load {Master_Report_Type} dataframe to MySQL ({Error_Message})",
             )
             Platforms_Not_Collected.append(Master_Report_Loading_Problem)
-            logging.info(f"Added to Platforms_Not_Collected: {SUSHI_Call_Data['JSON_Name']}|{Master_Report_Type}|Unable to load {Master_Report_Type} dataframe to MySQL")
+            logging.info(f"Added to Platforms_Not_Collected: {SUSHI_Call_Data['JSON_Name']}|{Master_Report_Type}|Unable to load {Master_Report_Type} dataframe to MySQL ({Error_Message})")
 
 #Section: Output Platforms_Not_Collected
 Platforms_Not_Collected_Location = str(Path('.', 'data', 'Platforms_Not_Collected.csv'))
